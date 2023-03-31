4dcad5be6ab1e2f37aa797b850c4bfe100e1d737
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-03-31 15:10:28 -0600

    Agregacion CRUD en client-service

--- service/client-service.js
+++ service/client-service.js
@@ -1,1 +1,18 @@
-console.log("CLIENTE-SERVICE");
+const http = new XMLHttpRequest();
+//abrir http (método, url)
+
+/*CRUD 
+    (Create, POST
+    Read, GET
+    Update, PUT/PATCH
+    Delete) DELETE
+*/
+//Métodos HTTP
+http.open("GET", "http://localhost:3000/perfil")
+http.send();
+http.onload = () => {
+    const data = http.response
+    console.log(data);
+}
+console.log(http);
+//console.log("CLIENTE-SERVICE");
