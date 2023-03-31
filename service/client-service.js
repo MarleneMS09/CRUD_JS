928a8d07a6b95545d0e91cd10fb94d5da8b8ef2b
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-03-31 17:26:30 -0600

    Se agrego CRUD

--- service/client-service.js
+++ service/client-service.js
@@ -1,3 +1,32 @@
+const crearNuevaLinea = (nombre,email) => {
+    const linea = document.createElement('tr')
+   const contenido =  `
+            <td class="td" data-td>
+            ${nombre}</td>
+            <td>${email}</td>
+            <td>
+              <ul class="table__button-control">
+                <li>
+                  <a
+                    href="../screens/editar_cliente.html"
+                    class="simple-button simple-button--edit"
+                    >Editar</a
+                  >
+                </li>
+                <li>
+                  <button
+                    class="simple-button simple-button--delete"
+                    type="button"
+                  >
+                    Eliminar
+                  </button>
+                </li>
+              </ul>
+            </td>
+          `;
+          linea.innerHTML = contenido 
+          return linea
+};
 const http = new XMLHttpRequest();
 //abrir http (método, url)
 
