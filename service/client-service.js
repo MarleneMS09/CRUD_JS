ab02e6cbd2d78f3d77afc35fbc4c1267232ae9ab
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-03-31 21:23:49 -0600

    Movilización consts CrearNuevaLinea y table (client-service)(client-controller), agregación CS y LC

--- service/client-service.js
+++ service/client-service.js
@@ -1,34 +1,3 @@
-//backticks
-const crearNuevaLinea = (nombre, email) => {
-  const linea = document.createElement("tr");
-  const contenido = `
-    <td class="td" data-td>
-      ${nombre}
-    </td>
-    <td>${email}</td>
-    <td>
-      <ul class="table__button-control">
-        <li>
-          <a
-            href="../screens/editar_cliente.html"
-            class="simple-button simple-button--edit"
-          >
-            Editar
-          </a>
-        </li>
-        <li>
-          <button class="simple-button simple-button--delete" type="button">
-            Eliminar
-          </button>
-        </li>
-      </ul>
-    </td>
-  `;
-  linea.innerHTML = contenido;
-  return linea;
-};
-
-const table = document.querySelector("[data-table]");
 
 //Abrir http (método,url)
 
@@ -39,7 +8,13 @@ const table = document.querySelector("[data-table]");
 // Delete - DELETE
 
 const listaClientes = () => {
-  const promise = new Promise((resolve, reject) => {
+    return fetch("http://localhost:3000/perfil").then(respuesta => respuesta.json())};
+
+export const clientServices = {
+    listaClientes 
+}
+
+  /*const promise = new Promise((resolve, reject) => {
     const http = new XMLHttpRequest();
     http.open("GET", "http://localhost:3000/perfil");
 
@@ -54,17 +29,9 @@ const listaClientes = () => {
       }
     };
   });
-  return promise;
-};
+  return promise; */
 
-listaClientes()
-  .then((data) => {
-    data.forEach((perfil) => {
-      const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
-      table.appendChild(nuevaLinea);
-    });
-  })
-  .catch((error) => alert("Ocurrió un error"));
+
 
 // console.log(data);
 //
