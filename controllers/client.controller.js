58ae3302885d5f422503ea6097a3ad8f3026e7b3
MarleneMS09 <marlenemsanchez05@gmail.com>
2023-04-01 14:46:35 -0600

    modificacion archivos

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -1,41 +1,54 @@
 import { clientServices } from "../service/client-service.js";
-console.log(clientServices);
 
-const crearNuevaLinea = (nombre, email) => {
-    const linea = document.createElement("tr");
-    const contenido = `
-      <td class="td" data-td>
-        ${nombre}
-      </td>
-      <td>${email}</td>
-      <td>
-        <ul class="table__button-control">
-          <li>
-            <a
-              href="../screens/editar_cliente.html"
-              class="simple-button simple-button--edit"
-            >
-              Editar
-            </a>
-          </li>
-          <li>
-            <button class="simple-button simple-button--delete" type="button">
-              Eliminar
-            </button>
-          </li>
-        </ul>
-      </td>
-    `;
-    linea.innerHTML = contenido;
-    return linea;
-  };
-  
-  const table = document.querySelector("[data-table]");
+//backticks
+const crearNuevaLinea = (nombre, email, id) => {
+  const linea = document.createElement("tr");
+  const contenido = `
+    <td class="td" data-td>
+      ${nombre}
+    </td>
+    <td>${email}</td>
+    <td>
+      <ul class="table__button-control">
+        <li>
+          <a
+            href="../screens/editar_cliente.html"
+            class="simple-button simple-button--edit"
+          >
+            Editar
+          </a>
+        </li>
+        <li>
+          <button class="simple-button simple-button--delete" type="button" id="${id}">
+            Eliminar
+          </button>
+        </li>
+      </ul>
+    </td>
+  `;
+  linea.innerHTML = contenido;
+  const btn = linea.querySelector("button");
+  btn.addEventListener("click", () => {
+    const id = btn.id;
+    clientServices
+      .eliminarCliente(id)
+      .then((respuesta) => {
+        console.log(respuesta);
+      })
+      .catch((err) => alert("Ocurrió un error"));
+  });
 
-   clientServices.listaClientes()
+  return linea;
+};
+
+const table = document.querySelector("[data-table]");
+
+clientServices
+  .listaClientes()
   .then((data) => {
-    data.forEach((perfil) => {
-      const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+    data.forEach(({ nombre, email, id }) => {
+      const nuevaLinea = crearNuevaLinea(nombre, email, id);
       table.appendChild(nuevaLinea);
     });
-  }).catch((error) => alert("Ocurrió un error"));
+  })
+  .catch((error) => alert("Ocurrió un error"));
--- service/client-service.js
+++ service/client-service.js
@@ -1,37 +1,24 @@
+const listaClientes = () =>
+  fetch("http://localhost:3000/perfil").then((respuesta) => respuesta.json());
 
-//Abrir http (método,url)
-
-// CRUD   - Métodos HTTP
-// Create - POST
-// Read   - GET
-// Update - PUT/PATCH
-// Delete - DELETE
-
-const listaClientes = () => {
-    return fetch("http://localhost:3001/perfil").then(respuesta => respuesta.json())};
-
-export const clientServices = {
-    listaClientes, 
+const crearCliente = (nombre, email) => {
+  return fetch("http://localhost:3000/perfil", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nombre, email, id: uuid.v4() }),
+  });
 };
 
-  /*const promise = new Promise((resolve, reject) => {
-    const http = new XMLHttpRequest();
-    http.open("GET", "http://localhost:3000/perfil");
-
-    http.send();
-
-    http.onload = () => {
-      const response = JSON.parse(http.response);
-      if (http.status >= 400) {
-        reject(response);
-      } else {
-        resolve(response);
-      }
-    };
+const eliminarCliente = (id) => {
+  return fetch(`http://localhost:3000/perfil/${id}`, {
+    method: "DELETE",
   });
-  return promise; */
+};
 
-
-
-// console.log(data);
-//
+export const clientServices = {
+  listaClientes,
+  crearCliente,
+  eliminarCliente,
+};
